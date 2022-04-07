9369f68e49c4a038d153a3e79321dc55ce36d445
vaishnavi754 <81965405+vaishnavipaliya11@users.noreply.github.com>
2022-04-07 15:10:27 +0530

    feat:quotes and images added

---
+++ src/Data.jsx
@@ -0,0 +1,33 @@
+const QuoteData = [
+  {
+    quote: " The purpose of our lives is to be happy. "
+    
+  },
+  {
+    quote: "life is unpredictable face it anyway"
+   
+  },
+  {
+    quote: "Turn your wounds into wisdom."
+  },
+  {
+    quote: "Work for your dream"
+    
+  },
+  {
+    quote:"honesty never goes out of style"
+  },
+  {
+    quote:
+      "be trustworthy"
+  },
+  {
+    quote:
+"Be attentive, Be strong"  },
+  {
+    quote:
+      "be trustworthy"
+  },
+];
+
+export { QuoteData };
---
+++ src/Home/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { QuoteData } from '../Data';
+import { useEffect } from 'react';
+
+const Home = () => {
+    const randomQuote = Math.floor(Math.random() * Math.floor(QuoteData.length));
+
+    useEffect(() => {
+        document.body.style.backgroundImage =
+          "url('https://source.unsplash.com/1600x900/?')";
+      }); 
+
+  return (
+    <div>
+    <h1>{QuoteData[randomQuote].quote}</h1>
+    </div>
+  )
+}
+
+export {Home}
